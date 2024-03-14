1d0107bdd9d93140a213c1fc598ed22e7cc45620
AmanNookat <amantur.mendibaev2003@gmail.com>
2024-03-14 10:33:43 +0600

    kchau

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite" // используется для создания объекта конфигурации
+import react from "@vitejs/plugin-react" // плагин, предназначенный для работы с React-приложениями
+import path from "path" // предоставляет утилиты для работы с путями к файлам и директориям
 
 export default defineConfig({
   plugins: [react()],
